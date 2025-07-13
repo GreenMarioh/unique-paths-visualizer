f9c257eb657ecea32042567ab0f8a43dc7865453
GreenMarioh <71488498+GreenMarioh@users.noreply.github.com>
2025-07-14 01:06:06 +0530

    Configured deployement files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/unique-paths-visualizer/', // Replace with your actual repo name
+  build: {
+    outDir: 'dist'
+  }
 })
